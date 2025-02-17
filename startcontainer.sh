86756d9af05af23fcda06e49408f0dd8684c9ce6
sagarvssv <46338541+sagarvssv@users.noreply.github.com>
2025-02-17 15:44:46 +0530

    Update startcontainer.sh

--- startcontainer.sh
+++ startcontainer.sh
@@ -6,7 +6,7 @@ echo "Starting Docker container"
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 010438505789.dkr.ecr.ap-south-1.amazonaws.com
 
 # Pull the latest Docker image
-docker pull 010438505789.dkr.ecr.ap-south-1.amazonaws.com/infraoutput_vcloud:latest
+docker pull 010438505789.dkr.ecr.ap-south-1.amazonaws.com/mallapp_vcloud:latest
 
 # Run the Docker container
-docker run -d -p 5000:5000 --name my-Infraoutputppapplication 010438505789.dkr.ecr.ap-south-1.amazonaws.com/infraoutput_vcloud:latest
+docker run -d -p 5000:5000 --name my-Infraoutputppapplication 010438505789.dkr.ecr.ap-south-1.amazonaws.com/mallapp_vcloud:latest
