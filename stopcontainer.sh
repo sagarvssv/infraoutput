e01289fd514d5da5899bd53ef1a7421e0f2dc14c
sagarvssv <46338541+sagarvssv@users.noreply.github.com>
2025-02-17 14:52:59 +0530

    update

---
+++ stopcontainer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Starting Docker container"
+
+# Authenticate with ECR
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 010438505789.dkr.ecr.ap-south-1.amazonaws.com
+
+# Pull the latest Docker image
+docker pull 010438505789.dkr.ecr.ap-south-1.amazonaws.com/mallapp_vcloud:latest
+
+# Run the Docker container
+docker run -d -p 5173:5173 --name my-Mallappapplication 010438505789.dkr.ecr.ap-south-1.amazonaws.com/mallapp_vcloud:latest
