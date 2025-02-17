13218194aa66fdb7ae55444ba8f304d1377f531d
sagarvssv <46338541+sagarvssv@users.noreply.github.com>
2025-02-17 15:46:34 +0530

    Update stopcontainer.sh

--- stopcontainer.sh
+++ stopcontainer.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Stopping and removing existing Docker container"
-docker stop my-Mallappapplication || true
-docker rm my-Mallappapplication || true
+docker stop my-Infraoutputppapplication  || true
+docker rm my-Infraoutputppapplication  || true
 
 echo "Removing old Docker image"
-docker rmi -f infraoutput_vcloud:latest || true
+docker rmi -f mallapp_vcloud:latest|| true
 
 echo "Old containers and images removed successfully."
