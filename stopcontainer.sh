e73566e32a08da41cfede8369c5d5a29667513f5
sagarvssv <46338541+sagarvssv@users.noreply.github.com>
2025-02-17 15:03:59 +0530

    Update stopcontainer.sh

--- stopcontainer.sh
+++ stopcontainer.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-echo "Starting Docker container"
+echo "Stopping and removing existing Docker container"
+docker stop my-Mallappapplication || true
+docker rm my-Mallappapplication || true
 
-# Authenticate with ECR
-aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 010438505789.dkr.ecr.ap-south-1.amazonaws.com
+echo "Removing old Docker image"
+docker rmi -f mallapp_vcloud:latest || true
 
-# Pull the latest Docker image
-docker pull 010438505789.dkr.ecr.ap-south-1.amazonaws.com/mallapp_vcloud:latest
-
-# Run the Docker container
-docker run -d -p 5000:5000 --name my-Mallappapplication 010438505789.dkr.ecr.ap-south-1.amazonaws.com/mallapp_vcloud:latest
+echo "Old containers and images removed successfully."
